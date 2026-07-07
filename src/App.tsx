e06af409304e05d522a2f4172f82f5da8bd06c67
Neel Patel <neel2911@users.noreply.github.com>
2026-07-06 23:23:51 -0400

    add container course notes

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import { HomePage } from '@/pages/HomePage'
 import { CoursePage } from '@/pages/CoursePage'
 import { SearchPage } from '@/pages/SearchPage'
 import { NotFoundPage } from '@/pages/NotFoundPage'
+import { TooltipProvider } from '@/components/ui/tooltip'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="course/:slug" element={<CoursePage />} />
-        <Route path="search" element={<SearchPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <TooltipProvider delayDuration={300}>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="course/:slug" element={<CoursePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </TooltipProvider>
   )
 }
 
